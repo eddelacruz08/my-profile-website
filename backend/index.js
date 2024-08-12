6ab74bcaad67d465866ebf5b0f33198b6c3186d6
eddelacruz08 <rhingmak21@gmail.com>
2024-08-12 14:43:46 +0800

    Update vercel.json

--- backend/index.js
+++ backend/index.js
@@ -76,8 +76,8 @@ app.use((req, res, next) => {
   next();
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
