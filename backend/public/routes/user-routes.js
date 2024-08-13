14eae904da471d11770cf05c785bc293d098602f
eddelacruz08 <rhingmak21@gmail.com>
2024-08-13 12:28:17 +0800

    fix import keys

--- backend/public/routes/user-routes.js
+++ backend/public/routes/user-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { MongoClient, ObjectId } = require('mongodb');
 const router = express.Router();
-const keys = require('./config/keys');
+const keys = require('./public/config/keys');
 
 let database;
 
