363fbe9731ad3b5683b743809202bb1d9b88edaf
eddelacruz08 <rhingmak21@gmail.com>
2024-08-13 13:49:45 +0800

    updated database file

--- backend/public/models/PageLink.js
+++ backend/public/models/PageLink.js
@@ -1,10 +1,8 @@
-const { Int32 } = require('mongodb');
 const mongoose = require('mongoose');
 
 const pageLinkSchema = new mongoose.Schema({
   description: String,
   name: String,
-  id: Int32,
   // Define other fields as needed
 });
 
